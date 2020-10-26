4383e7b49c9546545ea036325a1f87e9024bdefb
Jubin John <72947744+jubjohn@users.noreply.github.com>
2020-10-26 19:06:53 -0400

    Convert project to kubernetes

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React, straight from the horse's mouth
+            Learn React, straight from the horse's mouth v2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
