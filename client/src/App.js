dc618429724bb0ac3797862808b15f2dd96d2043
Jubin John <72947744+jubjohn@users.noreply.github.com>
2020-10-29 16:15:48 -0400

    Convert username of docker images

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React, straight from the horse's mouth v2
+            Learn React, straight from the horse's mouth Kubernetes!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
